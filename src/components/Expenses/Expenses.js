768c8e845f34f0032a0233c52c40d36d96e7b844
Nitin Ahuja <53057442+ahnitin@users.noreply.github.com>
2024-02-07 21:30:51 +0530

    Adding a filter which doesnt work on the expense tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card.js";
+import ExpenseFilter from "../NewExpense/ExpenseFilter.jsx";
 import "./Expenses.css";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpenseFilter>
       {props.expenses.map((expense) => (
         <ExpenseItem
           title={expense.title}
---
+++ src/components/NewExpense/ExpenseFilter.jsx
@@ -0,0 +1,21 @@
+import "./ExpenseFilter.css";
+const ExpenseFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    console.log(event.target.value);
+    props.onChangeFilter(event.target.value);
+  };
+  return (
+    <div className="expense-filter">
+      <div className="expense-filter__control"></div>
+      <label>Filter by Year</label>
+      <select value={props.selected} onChange={dropdownChangeHandler}>
+        <option value="2024">2024</option>
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+      </select>
+    </div>
+  );
+};
+export default ExpenseFilter;
